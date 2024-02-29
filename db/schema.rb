c50ad4170e8ea19edb245c5cfedc0e1e46875f60
shlomi fogel <mm12g1@gmail.com>
2024-02-29 14:53:25 +0100

    added address to bikes and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_120829) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_125204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_120829) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
